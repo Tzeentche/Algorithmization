5e2c51bf5213089afca6bc68bd2a4c6493f7f643
Tzeentche <ilyasugako87@gmail.com>
2019-07-12 09:17:27 +0300

    Added decision for 2.1.3

---
+++ src/edu/epam/oneDimentionalArrays/task4/MaxAndMin.java
@@ -0,0 +1,9 @@
+package edu.epam.oneDimentionalArrays.task4;
+
+//  Даны действительные числа а1 ,а2 ,..., аn . Поменять местами наибольший и наименьший элементы
+
+import java.util.*;
+import java.lang.*;
+
+public class MaxAndMin {
+}
