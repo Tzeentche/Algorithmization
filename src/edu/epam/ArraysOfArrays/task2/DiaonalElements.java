951ca1470c7cfd89ca3fa0c6a995263016bcd217
Tzeentche <ilyasugako87@gmail.com>
2019-07-14 17:14:27 +0300

    Add decision for task 2.2.2

---
+++ src/edu/epam/ArraysOfArrays/task2/DiaonalElements.java
@@ -0,0 +1,53 @@
+package edu.epam.ArraysOfArrays.task2;
+
+//  Дана квадратная матрица. Вывести на экран элементы, стоящие на диагонали
+
+import java.util.*;
+import java.lang.*;
+
+public class DiaonalElements {
+
+    public static void main(String[] args) {
+
+        int arraySizes = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        try{
+
+            System.out.println("Please,enter the size of User's array:");
+            arraySizes = input.nextInt();
+
+            int[][] squareMatrix = new int[arraySizes][arraySizes];
+
+            System.out.println("User's array is: ");
+
+            for (int lengthCounter = 0; lengthCounter < arraySizes; lengthCounter ++) {
+
+                for (int depthCounter = 0; depthCounter < arraySizes; depthCounter ++) {
+
+                    squareMatrix[lengthCounter][depthCounter] = (int)(Math.random() * 6 - 2);
+
+                    System.out.print(squareMatrix[lengthCounter][depthCounter] + " ");
+                }
+
+                System.out.println(" ");
+            }
+
+            for (int lengthCounter = 0; lengthCounter < arraySizes; lengthCounter ++) {
+
+                for (int depthCounter = 0; depthCounter < arraySizes; depthCounter ++) {
+
+                    if (lengthCounter == depthCounter) {
+
+                        System.out.println(squareMatrix[lengthCounter][depthCounter] + " ");
+                    }
+                }
+            }
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("You entered wrong walue. Let's try again!");
+        }
+    }
+}
---
+++ src/edu/epam/ArraysOfArrays/task3/StringByCall.java
@@ -0,0 +1,14 @@
+package edu.epam.ArraysOfArrays.task3;
+
+//Дана матрица. Вывести k-ю строку и p-й столбец матрицы
+
+import java.util.*;
+import java.lang.*;
+
+public class StringByCall {
+
+    public static void main(String[] args) {
+
+        int boofer = 0;
+    }
+}
