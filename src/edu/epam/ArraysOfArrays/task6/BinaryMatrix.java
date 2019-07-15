bf91565f2fab5e5abbebd6e2dfd33ee74e833749
Tzeentche <ilyasugako87@gmail.com>
2019-07-15 08:01:48 +0300

    Started development decision for task 2.2.6

---
+++ src/edu/epam/ArraysOfArrays/task6/BinaryMatrix.java
@@ -0,0 +1,59 @@
+package edu.epam.ArraysOfArrays.task6;
+
+//  Сформировать квадратную матрицу порядка n по заданному образцу(n - четное)
+
+import java.util.*;
+import java.lang.*;
+
+public class BinaryMatrix {
+
+    public static void main(String[] args) {
+
+        int size = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        try {
+
+            System.out.println("Please, enter EVEN size for the array: ");
+            size = input.nextInt();
+
+            if (size % 2 != 0) {
+
+                size += 1;
+                System.out.println("You entered non-even number. But we fixed it.");
+            }
+
+
+            System.out.println("Yours array has next view: ");
+            int[][] array = new int[size][size];
+
+            for (int firstCounter = 0; firstCounter < size; firstCounter ++) {
+
+                for (int secondCounter = 0; secondCounter < size; secondCounter ++) {
+
+                    if ((firstCounter == size) | (secondCounter == 0) | (firstCounter == 1) | (secondCounter == 1)) {
+
+                        array[firstCounter][secondCounter] = 1;
+                    } else {
+
+                        array[firstCounter][secondCounter] = 0;
+                    }
+
+                    if (secondCounter < size - 1) {
+
+                        System.out.print(array[firstCounter][secondCounter] + " ");
+
+                    } else {
+
+                        System.out.println(array[firstCounter][secondCounter]);
+                    }
+                }
+            }
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("You entered non-NUMERIC value.");
+        }
+    }
+}
