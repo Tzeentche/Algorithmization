8fd0b52b24df117b6436bb732020bcbd11217d23
Tzeentche <ilyasugako87@gmail.com>
2019-08-09 17:14:54 +0300

    Finished developing decision for task 2.2.9 with max column in positive matrix (EPAM, 2.Arrays, 2.Arrays of arrays)

--- src/edu/epam/ArraysOfArrays/task9/MaxColumnSumm.java
+++ src/edu/epam/ArraysOfArrays/task9/MaxColumnSumm.java
@@ -3,12 +3,82 @@ package edu.epam.ArraysOfArrays.task9;
 //Задана матрица неотрицательных чисел. Посчитать сумму элементов в каждом столбце. Определить, какой
 //        столбец содержит максимальную сумму.
 
-import java.lang.*;
+import jdk.internal.util.xml.impl.Input;
+
+import java.util.*;
 
 public class MaxColumnSumm {
 
     public static void main(String[] args) {
 
-        
+//        Declare the variables that we will use in the work:
+        int size = 0;
+        int max = 0;
+        String result = "";
+
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+        try {
+
+            System.out.println("Please, enter size of your array:");
+            size = input.nextInt();
+
+//            Declare boofer array for counting max.column values:
+
+            int[] boofer = new int[size];
+
+//            Fill the array with values:
+
+            System.out.println("Now the array looks like that:");
+            int[][] array = new int[size][size];
+
+            for (int firstCounter = 0; firstCounter < size; firstCounter++) {
+
+                for (int secondCounter = 0; secondCounter < size; secondCounter++) {
+
+                    array[firstCounter][secondCounter] = random.nextInt(10);
+
+                    System.out.print(array[firstCounter][secondCounter] + " ");
+
+                    boofer[secondCounter] += array[firstCounter][secondCounter];
+                }
+
+                System.out.println(" ");
+            }
+
+//            Output results of buffer's counter and finding column with max value:
+            System.out.println("Columns addition result is:");
+
+            max = boofer[0];
+            result = "1";
+
+            for (int firstIndex = 0; firstIndex < size; firstIndex++) {
+
+                System.out.print(boofer[firstIndex] + " ");
+
+                for (int secondIndex = 0; secondIndex < size; secondIndex++) {
+
+                    if (max < boofer[secondIndex]) {
+
+                        max = boofer[secondIndex];
+
+                        result = "" + (secondIndex + 1);
+                    }
+//
+//                    if (max == boofer[secondIndex]) {
+//
+//                        result += " and " + (secondIndex + 1);
+//                    }
+                }
+            }
+
+            System.out.println(" ");
+            System.out.println("Column with max value is " + result);
+
+        } catch(InputMismatchException e) {
+
+            System.out.println("You entered non-NAMERIC value! Try again next time...");
+        }
     }
 }
