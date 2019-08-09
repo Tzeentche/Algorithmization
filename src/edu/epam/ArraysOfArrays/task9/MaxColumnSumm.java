e5c24e507845464a5e0f2cc133795bef0c5f27f9
Tzeentche <ilyasugako87@gmail.com>
2019-08-09 15:42:45 +0300

    Started developing decision for task 2.2.9 with max column in positive matrix (EPAM, 2.Arrays, 2.Arrays of arrays)

---
+++ src/edu/epam/ArraysOfArrays/task9/MaxColumnSumm.java
@@ -0,0 +1,14 @@
+package edu.epam.ArraysOfArrays.task9;
+
+//Задана матрица неотрицательных чисел. Посчитать сумму элементов в каждом столбце. Определить, какой
+//        столбец содержит максимальную сумму.
+
+import java.lang.*;
+
+public class MaxColumnSumm {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
