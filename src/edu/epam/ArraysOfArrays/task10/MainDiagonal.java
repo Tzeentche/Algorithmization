99f3cec8ee52020507e18909142b477e70bc00ce
Tzeentche <ilyasugako87@gmail.com>
2019-08-10 14:05:36 +0300

    Started developing decision for task 2.2.10 with main diagonal positive elements (EPAM, 2.Arrays, 2.Arrays of arrays)

---
+++ src/edu/epam/ArraysOfArrays/task10/MainDiagonal.java
@@ -0,0 +1,13 @@
+package edu.epam.ArraysOfArrays.task10;
+
+//Найти положительные элементы главной диагонали квадратной матрицы
+
+import java.util.*;
+
+public class MainDiagonal {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
