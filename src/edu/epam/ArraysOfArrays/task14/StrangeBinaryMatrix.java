48f1ef2480631b21edd40019acd38003488c96e5
Tzeentche <ilyasugako87@gmail.com>
2019-08-11 22:44:18 +0300

    Started developing decision for task 2.2.14 with binary matrix with 0/1 values (EPAM, 2.Arrays, 2.Arrays of arrays)

---
+++ src/edu/epam/ArraysOfArrays/task14/StrangeBinaryMatrix.java
@@ -0,0 +1,7 @@
+package edu.epam.ArraysOfArrays.task14;
+
+//      Сформировать случайную матрицу m x n, состоящую из нулей и единиц, причем в каждом столбце число
+//      единиц равно номеру столбца
+
+public class StrangeBinaryMatrix {
+}
