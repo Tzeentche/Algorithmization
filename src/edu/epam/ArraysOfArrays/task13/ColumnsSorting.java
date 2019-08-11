83fe461e299b73642308469c0efb7b2623d18be5
Tzeentche <ilyasugako87@gmail.com>
2019-08-11 22:25:05 +0300

    Finished developing decision for task 2.2.13 with up- and down-sorting of array columns (EPAM, 2.Arrays, 2.Arrays of arrays)

--- src/edu/epam/ArraysOfArrays/task13/ColumnsSorting.java
+++ src/edu/epam/ArraysOfArrays/task13/ColumnsSorting.java
@@ -8,6 +8,82 @@ public class ColumnsSorting {
 
     public static void main(String[] args) {
 
-        
+//        Declare mostly using variables:
+
+        int buffer = 0;
+        int size = 5;
+        int[][] array = new int[size][size];
+
+        Random rnd = new Random();
+
+//        We did not receive a specific task on the size of the array and how to fill it, so it will be
+//        a randomly filling array of 5x5 size:
+
+        System.out.println("User's array looks like that: ");
+
+        try {
+
+            for(int firstCounter = 0; firstCounter < size; firstCounter ++) {
+
+                for (int secondCounter = 0; secondCounter < size; secondCounter ++) {
+
+                    array[firstCounter][secondCounter] = rnd.nextInt(11);
+
+                    System.out.print(array[firstCounter][secondCounter] + " ");
+                }
+
+                System.out.println(" ");
+            }
+
+//            We make UP- and DOWN-sorting the array's columns by Bubble method:
+
+            System.out.println("Array with UP-sorting columns looks like that: ");
+
+            for (int rowSorting = 0; rowSorting < size; rowSorting ++) {
+
+                for (int sorting = 0; sorting < size; sorting ++) {
+
+                    for (int enumenator = 0; enumenator < size - sorting - 1; enumenator ++) {
+
+                        if (array[enumenator][rowSorting] > array[enumenator + 1][rowSorting]) {
+
+                            buffer = array[enumenator + 1][rowSorting];
+                            array[enumenator + 1][rowSorting] = array[enumenator][rowSorting];
+                            array[enumenator][rowSorting] = buffer;
+                        }
+                    }
+
+                    System.out.print(array[rowSorting][sorting] + " ");
+                }
+
+                System.out.println(" ");
+            }
+
+            System.out.println("Array with DOWN-sorting columns looks like that: ");
+
+            for (int rowSorting = 0; rowSorting < size; rowSorting ++) {
+
+                for (int sorting = 0; sorting < size; sorting ++) {
+
+                    for (int enumenator = 0; enumenator < size - sorting - 1; enumenator ++) {
+
+                        if (array[enumenator][rowSorting] < array[enumenator + 1][rowSorting]) {
+
+                            buffer = array[enumenator + 1][rowSorting];
+                            array[enumenator + 1][rowSorting] = array[enumenator][rowSorting];
+                            array[enumenator][rowSorting] = buffer;
+                        }
+                    }
+
+                    System.out.print(array[rowSorting][sorting] + " ");
+                }
+
+                System.out.println(" ");
+            }
+
+        } catch(ArrayIndexOutOfBoundsException e) {
+
+            System.out.println("There are too many values here. As a result, some data went beyond the array");
+        }
     }
 }
