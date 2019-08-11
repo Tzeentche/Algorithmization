903caee404b31fb03b885293f7931185587ce519
Tzeentche <ilyasugako87@gmail.com>
2019-08-11 23:53:05 +0300

    Started developing decision for task 2.2.15 with max element in the array of arrays (EPAM, 2.Arrays, 2.Arrays of arrays)

---
+++ src/edu/epam/ArraysOfArrays/task15/MaxArrayElement.java
@@ -0,0 +1,13 @@
+package edu.epam.ArraysOfArrays.task15;
+
+//Найдите наибольший элемент матрицы и заменить все нечетные элементы на него.
+
+import java.util.*;
+
+public class MaxArrayElement {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
