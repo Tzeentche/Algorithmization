2754ac2fd116fb461023cb21ca11af47436d324d
Tzeentche <ilyasugako87@gmail.com>
2019-08-11 23:49:55 +0300

    Finished developing decision for task 2.2.14 with binary matrix with 0/1 values (EPAM, 2.Arrays, 2.Arrays of arrays)

--- src/edu/epam/ArraysOfArrays/task14/StrangeBinaryMatrix.java
+++ src/edu/epam/ArraysOfArrays/task14/StrangeBinaryMatrix.java
@@ -3,5 +3,55 @@ package edu.epam.ArraysOfArrays.task14;
 //      Сформировать случайную матрицу m x n, состоящую из нулей и единиц, причем в каждом столбце число
 //      единиц равно номеру столбца
 
+import java.util.*;
+
 public class StrangeBinaryMatrix {
+
+    public static void main(String[] args) {
+
+//        Initialize values, that's we use in our program:
+
+        int buffer = 0;
+        int m;
+        int n;
+
+        Random rnd = new Random();
+
+        System.out.println("This array looks like the next: ");
+        m = (rnd.nextInt(3) + 2);
+        n = m + 1;
+
+        System.out.println("Value of columns is: " + m);
+        System.out.println("Value of rows is: " + n);
+
+        int[][] array = new int[n][m];
+
+        try {
+
+            for (int firstCounter = 0; firstCounter < n; firstCounter ++) {
+
+                for (int secondCounter = 0; secondCounter < m; secondCounter ++) {
+
+                    if (buffer < firstCounter) {
+
+                        array[firstCounter][secondCounter] = 0;
+
+                        buffer += 1;
+
+                    } else {
+
+                        array[firstCounter][secondCounter] = 1;
+                    }
+
+                    System.out.print(array[firstCounter][secondCounter] + " ");
+                }
+
+                System.out.println(" ");
+                buffer = 0;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+            System.out.println("It's so awful, but your values fellout of the array. Try again another time.");
+        }
+    }
 }
