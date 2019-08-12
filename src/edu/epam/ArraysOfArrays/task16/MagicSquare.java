1c5aea8325ce803a6edad49c0b59c807e7b8db0e
Tzeentche <ilyasugako87@gmail.com>
2019-08-12 16:19:45 +0300

    Developing decision for for task 2.2.16 "Magic square" for odd-size matrix (EPAM, 2.Arrays, 2.Arrays of arrays)

--- src/edu/epam/ArraysOfArrays/task16/MagicSquare.java
+++ src/edu/epam/ArraysOfArrays/task16/MagicSquare.java
@@ -1,4 +1,32 @@
 package edu.epam.ArraysOfArrays.task16;
 
+//      Магическим квадратом порядка n называется квадратная матрица размера nxn, составленная из чисел 1, 2, 3,
+//      ..., 2 n так, что суммы по каждому столбцу, каждой строке и каждой из двух больших диагоналей равны между
+//      собой. Построить такой квадрат.
+
+import java.util.*;
+
 public class MagicSquare {
+
+    public static void main(String[] args) {
+
+//        Declare values, we using in decision:
+
+        int buffer = 0;
+        Random rnd = new Random();
+        Scanner input = new Scanner(System.in);
+        int size = 3;
+        int[][] array = new int[size][size];
+        int numbers = size * size;
+
+//        Create array:
+
+//        for (int firstCounter = 0; firstCounter < size; firstCounter ++) {
+//
+//            for (int secondCounter = 0; secondCounter < size; secondCounter ++) {
+//
+//                array[firstCounter][secondCounter] =
+//            }
+//        }
+    }
 }
