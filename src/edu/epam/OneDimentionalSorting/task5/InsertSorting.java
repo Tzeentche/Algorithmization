ea3b91204c30c9a6b93fbbea17423b425e2fc551
Tzeentche <ilyasugako87@gmail.com>
2019-08-14 23:15:59 +0300

    Finished development decision for task 2.3.5 "Insert sorting and Binary search" (EPAM, 2.Arrays, 3.One dimension arrays sorting)

--- src/edu/epam/OneDimentionalSorting/task5/InsertSorting.java
+++ src/edu/epam/OneDimentionalSorting/task5/InsertSorting.java
@@ -7,5 +7,101 @@ package edu.epam.OneDimentionalSorting.task5;
 //        не будут перебраны. Примечание. Место помещения очередного элемента в отсортированную часть производить
 //        с помощью двоичного поиска. Двоичный поиск оформить в виде отдельной функции.
 
+import java.util.*;
+
 public class InsertSorting {
+
+    public static void main(String[] args) {
+
+//        Declare vales:
+
+        int size;
+        Random rnd = new Random();
+        Scanner input = new Scanner(System.in);
+        int[] array;
+        int secondCounter, index = 0, newValue = 0;
+
+        try {
+
+//            Input the size of the array and fill it with random values. Sorting 1/2 array, output result:
+
+            System.out.println("Please enter a numerical value, that will indicate the size of the array: ");
+            size = input.nextInt();
+            array = new int[size];
+
+            for (int firstCounter = 0; firstCounter < size; firstCounter ++) {
+
+                array[firstCounter] = rnd.nextInt(11);
+
+                System.out.print(array[firstCounter] + " ");
+            }
+
+            System.out.println(" ");
+
+            for (secondCounter = 1; secondCounter < (size / 2); secondCounter ++) {
+
+                newValue = array[secondCounter];
+                index = secondCounter;
+
+                while (index > 0 && array[index - 1] > newValue)
+                {
+
+                    array[index] = array[index - 1];
+                    index--;
+                }
+
+                array[index] = newValue;
+
+            }
+
+            System.out.println("Result of sorting first 1/2 of array: ");
+
+            for (int firstCounter = 0; firstCounter < size; firstCounter ++) {
+
+                System.out.print(array[firstCounter] + " ");
+            }
+
+            System.out.println(" ");
+
+//                Sorting second 1/2 array with binary search:
+
+            binarySorting(size, secondCounter, newValue, index, array);
+
+        } catch(InputMismatchException e) {
+
+            System.out.println("Your value was non-numeric. You can try again next time.");
+
+        } catch(ArrayIndexOutOfBoundsException e2) {
+
+            System.out.println("Something wrong happened, and amount of data exceeded the size of the array.");
+        }
+    }
+
+    public static void binarySorting(int size, int secondCounter, int newValue, int index, int[] array){
+
+        for (secondCounter = (size / 2); secondCounter < size; secondCounter ++) {
+
+            newValue = array[secondCounter];
+            index = secondCounter;
+
+            while (index > 0 && array[index - 1] > newValue)
+            {
+
+                array[index] = array[index - 1];
+                index--;
+            }
+
+            array[index] = newValue;
+        }
+
+        System.out.println("Result of sorting second 1/2 of array: ");
+
+        for (int firstCounter = 0; firstCounter < size; firstCounter ++) {
+
+            System.out.print(array[firstCounter] + " ");
+        }
+
+        System.out.println(" ");
+
+    }
 }
