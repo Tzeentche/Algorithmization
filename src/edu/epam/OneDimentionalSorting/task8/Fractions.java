20bbdb3636ab976769e8042231c35a5cdd2e7939
Tzeentche <ilyasugako87@gmail.com>
2019-08-16 23:14:07 +0300

    Started developing decision for task 2.3.8 "Fractions sorting" (EPAM, 2.Arrays, 3.One dimension arrays sorting)

---
+++ src/edu/epam/OneDimentionalSorting/task8/Fractions.java
@@ -0,0 +1,12 @@
+package edu.epam.OneDimentionalSorting.task8;
+
+//        Даны дроби. Составить программу, которая приводит эти дроби к общему
+//        знаменателю и упорядочивает их в порядке возрастания.
+
+public class Fractions {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
