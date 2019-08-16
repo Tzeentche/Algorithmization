c27280545501957f11a2935f0979fd4194b15934
Tzeentche <ilyasugako87@gmail.com>
2019-08-16 23:20:18 +0300

    Started development decision for task 2.4.1 "Greatest common factor" (EPAM, 2.Arrays, 4.Decomposition with methods)

---
+++ src/edu/epam/Decomposition/task1/GreatestCommonFactor.java
@@ -0,0 +1,12 @@
+package edu.epam.Decomposition.task1;
+
+//        Написать метод(методы) для нахождения наибольшего общего делителя и наименьшего общего кратного двух
+//        натуральных чисел
+
+public class GreatestCommonFactor {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
