4b5774008683f8596f94c1f448dd527337afb5c8
Tzeentche <ilyasugako87@gmail.com>
2019-08-17 23:57:57 +0300

    Started developing decision for task 2.4.5 "Second max value" (EPAM, 2.Arrays, 3.One dimension arrays sorting)

---
+++ src/edu/epam/Decomposition/task5/SecondMaxNumber.java
@@ -0,0 +1,11 @@
+package edu.epam.Decomposition.task5;
+
+//      Составить программу, которая в массиве A[N] находит второе по величине число (вывести на печать число,
+//      которое меньше максимального элемента массива, но больше всех других элементов)
+
+public class SecondMaxNumber {
+
+    public static void main(String[] args) {
+        
+    }
+}
