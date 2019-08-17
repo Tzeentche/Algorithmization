431bf8569dffb02590d302304735bbc0994fe4b6
Tzeentche <ilyasugako87@gmail.com>
2019-08-17 23:48:34 +0300

    Started developing decision for task 2.4.3 "Square of sixangle" (EPAM, 2.Arrays, 3.One dimension arrays sorting)

---
+++ src/edu/epam/Decomposition/task3/SixAngle.java
@@ -0,0 +1,10 @@
+package edu.epam.Decomposition.task3;
+
+//      Вычислить площадь правильного шестиугольника со стороной а, используя метод вычисления площади треугольника
+
+public class SixAngle {
+    public static void main(String[] args) {
+
+        
+    }
+}
