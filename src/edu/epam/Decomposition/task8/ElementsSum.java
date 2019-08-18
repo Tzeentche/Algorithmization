9f4ee053c32f0cc6c4a875d957ad6f5aae4a0f12
Tzeentche <ilyasugako87@gmail.com>
2019-08-18 23:40:40 +0300

    Started developing decision for task 2.4.8 "Elements summ" (EPAM, 2.Arrays, 4.Decomposition)

---
+++ src/edu/epam/Decomposition/task8/ElementsSum.java
@@ -0,0 +1,14 @@
+package edu.epam.Decomposition.task8;
+
+//        Задан массив D. Определить следующие суммы: D[l] + D[2] + D[3]; D[3] + D[4] + D[5]; D[4] +D[5] +D[6].
+//        Пояснение. Составить метод(методы) для вычисления суммы трех последовательно расположенных элементов
+//        массива с номерами от k до m.
+
+import java.util.*;
+
+public class ElementsSum {
+
+    public static void main(String[] args) {
+        
+    }
+}
