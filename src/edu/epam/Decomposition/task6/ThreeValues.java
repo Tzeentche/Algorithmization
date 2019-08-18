0bda18999eb33beec3b8f476d84c8645b0e0680c
Tzeentche <ilyasugako87@gmail.com>
2019-08-18 23:31:56 +0300

    Started developing decision for task 2.4.6 "Three simple numbers" (EPAM, 2.Arrays, 4.Decomposition)

---
+++ src/edu/epam/Decomposition/task6/ThreeValues.java
@@ -0,0 +1,12 @@
+package edu.epam.Decomposition.task6;
+
+//      Написать метод(методы), проверяющий, являются ли данные три числа взаимно простыми.
+
+import java.util.*;
+
+public class ThreeValues {
+
+    public static void main(String[] args) {
+        
+    }
+}
