7922f94b8c0c9ffd7590574b9b6d00906fc93e82
Tzeentche <ilyasugako87@gmail.com>
2019-08-18 23:34:14 +0300

    Started developing decision for task 2.4.7 "Factorial negative numbers" (EPAM, 2.Arrays, 4.Decomposition)

---
+++ src/edu/epam/Decomposition/task7/NegativeFactorial.java
@@ -0,0 +1,12 @@
+package edu.epam.Decomposition.task7;
+
+//      Написать метод(методы) для вычисления суммы факториалов всех нечетных чисел от 1 до 9.
+
+import java.util.*;
+
+public class NegativeFactorial {
+
+    public static void main(String[] args) {
+        
+    }
+}
