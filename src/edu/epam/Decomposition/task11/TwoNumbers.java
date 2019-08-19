b82f5bec77dd9b22dd07d44ebd9f5684214bf644
Tzeentche <ilyasugako87@gmail.com>
2019-08-19 23:45:16 +0300

    Started developing decision for task 2.4.11 "Value with max numbers" (EPAM, 2.Arrays, 4.Decomposition)

---
+++ src/edu/epam/Decomposition/task11/TwoNumbers.java
@@ -0,0 +1,13 @@
+package edu.epam.Decomposition.task11;
+
+//      Написать метод(методы), определяющий, в каком из данных двух чисел больше цифр.
+
+import java.util.*;
+
+public class TwoNumbers {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
