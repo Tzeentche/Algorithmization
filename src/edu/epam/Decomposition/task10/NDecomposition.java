98e76ac38dd1e63fe7ae61eef05c86c2a5721f12
Tzeentche <ilyasugako87@gmail.com>
2019-08-19 23:43:10 +0300

    Started developing decision for task 2.4.10 "Decomposition of N" (EPAM, 2.Arrays, 4.Decomposition)

---
+++ src/edu/epam/Decomposition/task10/NDecomposition.java
@@ -0,0 +1,14 @@
+package edu.epam.Decomposition.task10;
+
+//      Дано натуральное число N. Написать метод(методы) для формирования массива,
+//      элементами которого являются цифры числа N.
+
+import java.util.*;
+
+public class NDecomposition {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
