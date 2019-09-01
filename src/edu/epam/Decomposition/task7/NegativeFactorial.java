aa20343f446020c32aa6df2a608b7a215d3ffc33
Tzeentche <ilyasugako87@gmail.com>
2019-09-01 23:57:10 +0300

    Finished development decision for task 2.3.7 "Factorial oddnumbers 1 to 9" (EPAM, 2.Arrays, 3.One dimension arrays sorting)

--- src/edu/epam/Decomposition/task7/NegativeFactorial.java
+++ src/edu/epam/Decomposition/task7/NegativeFactorial.java
@@ -6,7 +6,26 @@ import java.util.*;
 
 public class NegativeFactorial {
 
+    int boofer = 1;
+
     public static void main(String[] args) {
-        
+
+        NegativeFactorial result = new NegativeFactorial();
+        int factSumm = result.calculator();
+
+        System.out.println("Factorial odd numbers 1 to 9 is " + factSumm);
     }
+
+    int calculator() {
+
+        for (int counter = 1; counter < 10; counter += 2) {
+
+            boofer = boofer * counter;
+
+            System.out.println("Factorial " + counter + " is " + boofer);
+        }
+
+        return boofer;
+    }
+
 }
