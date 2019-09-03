4e2490f365ac63ebb8d2d3736dc2b6af0442d358
Tzeentche <ilyasugako87@gmail.com>
2019-09-03 23:44:48 +0300

    Started development decision for task 2.4.12 "Create array A with elements of K less than N" (EPAM, 2.Arrays, 4.Decomposition)

---
+++ src/edu/epam/Decomposition/task12/arrayAKN.java
@@ -0,0 +1,13 @@
+package edu.epam.Decomposition.task12;
+
+//      Даны натуральные числа К и N. Написать метод(методы) формирования массива А, элементами которого
+//      являются числа, сумма цифр которых равна К и которые не большее N.
+
+import java.util.*;
+
+public class arrayAKN {
+
+    public static void main(String[] args) {
+        
+    }
+}
