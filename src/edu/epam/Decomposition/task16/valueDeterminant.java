fa938737dc57b9ae1b114fe9c58318e3dbb85147
Tzeentche <ilyasugako87@gmail.com>
2019-09-06 23:04:54 +0300

    Started development decision for task 2.4.16 "Value summ determinant" (EPAM, 2.Arrays)

---
+++ src/edu/epam/Decomposition/task16/valueDeterminant.java
@@ -0,0 +1,14 @@
+package edu.epam.Decomposition.task16;
+
+//      Написать программу, определяющую сумму n - значных чисел, содержащих только нечетные цифры. Определить также,
+//      сколько четных цифр в найденной сумме. Для решения задачи использовать декомпозицию.
+
+import java.util.*;
+
+public class valueDeterminant {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
