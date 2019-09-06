19f9b0c80465698564fdfa485cc3ef5210f1c4e5
Tzeentche <ilyasugako87@gmail.com>
2019-09-07 00:22:59 +0300

    Added methods to decision for task 2.4.4 "Determine the maximum distance" (EPAM, 2.Arrays)

--- src/edu/epam/Decomposition/task4/MaxDistance.java
+++ src/edu/epam/Decomposition/task4/MaxDistance.java
@@ -8,4 +8,39 @@ public class MaxDistance {
     public static void main(String[] args) {
 
     }
+
+    int firstPoint() {
+
+        return 0;
+    }
+
+    int secondPoint() {
+
+        return 0;
+    }
+
+    int thirdPoint() {
+
+        return 0;
+    }
+
+    int fourthPoint() {
+
+        return 0;
+    }
+
+    int distanceMeter() {
+
+        return 0;
+    }
+
+    int arrayCollector() {
+
+        return 0;
+    }
+
+    int output() {
+
+        return 0;
+    }
 }
