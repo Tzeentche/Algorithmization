7799e0d422479cd0bd56eec9cb71b7bd700e6c2e
Tzeentche <ilyasugako87@gmail.com>
2019-09-08 23:32:59 +0300

    Added methods to decision for task 2.4.15 "Numbers grow up, find all" (EPAM, 2.Arrays)

--- src/edu/epam/Decomposition/task15/growValue.java
+++ src/edu/epam/Decomposition/task15/growValue.java
@@ -3,7 +3,31 @@ package edu.epam.Decomposition.task15;
 //      Найти все натуральные n-значные числа, цифры в которых образуют строго возрастающую последовательность
 //      (например, 1234, 5789). Для решения задачи использовать декомпозицию.
 
+import java.util.*;
+
 public class growValue {
 
-    
+    public static void main(String[] args) {
+
+    }
+
+    int limitN() {
+
+        return 0;
+    }
+
+    int valueSeparator() {
+
+        return 0;
+    }
+
+    int comparator() {
+
+        return 0;
+    }
+
+    int output() {
+
+        return 0;
+    }
 }
