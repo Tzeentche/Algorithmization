909c02932ccb9cd6e3e46c3670067be278975fd3
Tzeentche <ilyasugako87@gmail.com>
2019-09-09 23:06:11 +0300

    Added methods to decision for task 2.4.17 "Subtract summ from value" (EPAM, 2.Arrays)

--- src/edu/epam/Decomposition/task17/toZero.java
+++ src/edu/epam/Decomposition/task17/toZero.java
@@ -3,10 +3,31 @@ package edu.epam.Decomposition.task17;
 //      Из заданного числа вычли сумму его цифр. Из результата вновь вычли сумму его цифр и т.д. Сколько таких действий
 //      надо произвести, чтобы получился нуль? Для решения задачи использовать декомпозицию.
 
+import java.util.*;
+
 public class toZero {
 
     public static void main(String[] args) {
 
-        
+    }
+
+    int valueContainer() {
+
+        return 0;
+    }
+
+    int valueSeparator() {
+
+        return 0;
+    }
+
+    int valueSubstraction() {
+
+        return 0;
+    }
+
+    int output() {
+
+        return 0;
     }
 }
